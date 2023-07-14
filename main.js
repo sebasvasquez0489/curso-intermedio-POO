b81c0306061b0a3d339c6ff86d34255570210dc6
Sebastian Vasquez <sebitas0489@gmail.com>
2023-07-13 22:50:35 -0500

    Metodos estáticos del prototipo Object

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("HELLO");
+const sebastian = {
+  name: "Sebas",
+  age: 34,
+  approvedCourses: ["Curso 1"],
+  addCourse(newCourse) {
+    console.log("This", this);
+    console.log("This.approvedCourses", this.approvedCourses);
+    // `this` se usa para hacer referencia a la propiedad `approvedCourses`
+    // del objeto en cuestión.
+    this.approvedCourses.push(newCourse);
+  },
+};
+
+// console.log(Object.keys(sebastian));
+// console.log(Object.getOwnPropertyNames(sebastian));
+// console.log(Object.entries(sebastian));
+
+console.log(Object.getOwnPropertyDescriptors(sebastian));
