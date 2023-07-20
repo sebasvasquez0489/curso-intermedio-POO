c14abb3a4b839b5ab9811101387847fcdd4a1156
Sebastian Vasquez <sebitas0489@gmail.com>
2023-07-19 23:24:05 -0500

    Ejemplo: Duck Typing

--- factoryPatter-RORO.js
+++ factoryPatter-RORO.js
@@ -26,23 +26,33 @@ function createStudent({
       instagram,
       facebook,
     },
-    readName() {
+    get name() {
       return private["_name"];
     },
-    changeName(newName) {
-      private["_name"] = newName;
+    set name(newName) {
+      if (newName.length != 0) {
+        private["_name"] = newName;
+      } else {
+        console.warn("Tu nombre debe tener al menos un caracter");
+      }
     },
+    // readName() {
+    //   return private["_name"];
+    // },
+    // changeName(newName) {
+    //   private["_name"] = newName;
+    // },
   };
 
-  Object.defineProperty(public, "readName", {
-    writable: false,
-    configurable: false,
-  });
+  // Object.defineProperty(public, "readName", {
+  //   writable: false,
+  //   configurable: false,
+  // });
 
-  Object.defineProperty(public, "changeName", {
-    writable: false,
-    configurable: false,
-  });
+  // Object.defineProperty(public, "changeName", {
+  //   writable: false,
+  //   configurable: false,
+  // });
 
   return public;
 }
