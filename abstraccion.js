f4ff17179535a0c32a50c38fb182cd56051efac1
Sebastian Vasquez <sebitas0489@gmail.com>
2023-07-19 21:44:36 -0500

    Ejemplo: Abstraccion con objetos literales y deep copy

---
+++ abstraccion.js
@@ -0,0 +1,71 @@
+function isObject(subject) {
+  return typeof subject == "object";
+}
+
+function isArray(subject) {
+  return Array.isArray(subject);
+}
+
+/// CREAMOS FUNCION QUE RECIBE UN ELEMENTO , STRING, NUM, BOOLEAN,ETC ////
+function deepCopy(subject) {
+  let copySubject;
+
+  /// VALIDAMOS SI ES OBJETO O ARRAY ////
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  /// EJECUTAMOS CON EL CICLO "FOR" POR CADA UNO DE LOS ELEMENTOS DE LAS PROPIEDADES DEL ELELMENTO ////
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
+}
+
+//*** CREAMOS LA CLASE ***//
+
+const studentBase = {
+  name: undefined,
+  email: undefined,
+  age: undefined,
+  approvedCourses: undefined,
+  learningPaths: undefined,
+  socialMedia: {
+    twitter: undefined,
+    instagram: undefined,
+    facebook: undefined,
+  },
+};
+
+/// CREAMOS ESTUDIANTES ////
+
+const sebastian = deepCopy(studentBase);
+//*** MODIFICAR PROPIEDAD PERO NO PERMITE ELIMINARLA ***/
+//SOLUCION 1
+Object.seal(sebastian);
+// SOLUCION 2
+// Object.defineProperty(sebastian, "name", {
+//   value: "sebas",
+//   configurable: false,
+// });
+
+//Object.seal(obj) se usa para evitar borrar propiedades
+//Object.freeze(obj) se usa para evitar borrar y editar una propiedad
